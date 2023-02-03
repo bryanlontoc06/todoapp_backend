7ffa8e05947009e71a4d61b1cc5eb20edbc1f649
Bry <bryanlontoc06@gmail.com>
2023-02-04 00:40:54 +0800

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework', 
     'rest_framework.authtoken', 
     'todo_api'
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,6 +118,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 
 # Static files (CSS, JavaScript, Images)
